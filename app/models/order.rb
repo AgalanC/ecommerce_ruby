d6d364a8359780117e0b5e8275054cb5a2d94ab1
AgalanC <galanc.armando@gmail.com>
2024-07-17 18:57:02 -0500

    Added functionality to save and track customer's orders in ActiveAdmin.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,12 @@ class Order < ApplicationRecord
   has_many :beers, through: :order_items
 
   validates :user_id, :tax_id, presence: true # Ensure foreign keys are present
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "final_price", "id", "status", "tax_id", "total_price", "total_tax", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["beers", "order_items", "tax", "user"]
+  end
 end
